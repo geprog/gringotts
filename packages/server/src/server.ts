70af9ae6829d35bca7e01d9e17b2628bda066a9c
Anbraten <anton@ju60.de>
2022-07-24 22:11:37 +0200

    feat: add customer endpoints and improve schema

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -4,7 +4,7 @@ import fastify, { FastifyInstance } from 'fastify';
 
 import { config } from '~/config';
 import { database } from '~/database';
-import { Subscription } from '~/entities';
+import { Customer, Subscription } from '~/entities';
 import dayjs from '~/lib/dayjs';
 import { Mollie } from '~/providers/mollie';
 import { PaymentProvider } from '~/providers/types';
@@ -23,11 +23,103 @@ export async function init(): Promise<FastifyInstance> {
         version: '0.1.0',
       },
       host: 'localhost:3000',
-      schemes: ['http'],
+      schemes: ['http', 'https'],
       consumes: ['application/json'],
       produces: ['application/json'],
     },
     exposeRoute: true,
+  });
+
+  server.addSchema({
+    $id: 'responses/success',
+    type: 'object',
+    properties: {
+      ok: { type: 'boolean' },
+    },
+  });
+
+  server.addSchema({
+    $id: 'responses/error',
+    type: 'object',
+    properties: {
+      error: { type: 'string' },
+    },
+  });
+
+  server.addSchema({
+    $id: 'entities/invoice',
+    type: 'object',
+    properties: {
+      items: {
+        type: 'array',
+        items: {
+          type: 'object',
+          properties: {
+            start: { type: 'string' },
+            end: { type: 'string' },
+            units: { type: 'number' },
+            pricePerUnit: { type: 'number' },
+          },
+        },
+      },
+      start: { type: 'string' },
+      end: { type: 'string' },
+    },
+  });
+
+  server.addSchema({
+    $id: 'entities/customer',
+    type: 'object',
+    properties: {
+      _id: { type: 'string' },
+      email: { type: 'string' },
+      name: { type: 'string' },
+    },
+  });
+
+  server.addSchema({
+    $id: 'entities/subscription_change',
+    type: 'object',
+    properties: {
+      _id: { type: 'string' },
+      start: { type: 'string' },
+      end: { type: 'string' },
+      pricePerUnit: { type: 'number' },
+      units: { type: 'number' },
+    },
+  });
+
+  server.addSchema({
+    $id: 'entities/subscription',
+    type: 'object',
+    properties: {
+      _id: { type: 'string' },
+      anchorDate: { type: 'string' },
+      lastPayment: { type: 'string' },
+      // customer: { $ref: 'entities/customer' },
+      customer: {
+        type: 'object',
+        properties: {
+          _id: { type: 'string' },
+          email: { type: 'string' },
+          name: { type: 'string' },
+        },
+      },
+      changes: {
+        type: 'array',
+        // items: { $ref: 'entities/subscription_change' },
+        items: {
+          type: 'object',
+          properties: {
+            _id: { type: 'string' },
+            start: { type: 'string' },
+            end: { type: 'string' },
+            pricePerUnit: { type: 'number' },
+            units: { type: 'number' },
+          },
+        },
+      },
+    },
   });
 
   await server.register(fastifyFormBody);
@@ -54,16 +146,10 @@ export async function init(): Promise<FastifyInstance> {
           },
         },
         400: {
-          type: 'object',
-          properties: {
-            error: { type: 'string' },
-          },
+          $ref: 'responses/error',
         },
         404: {
-          type: 'object',
-          properties: {
-            error: { type: 'string' },
-          },
+          $ref: 'responses/error',
         },
       },
     },
@@ -121,7 +207,7 @@ export async function init(): Promise<FastifyInstance> {
     },
   });
 
-  server.post('/subscription/:subscriptionId/change', {
+  server.post('/subscription/:subscriptionId', {
     schema: {
       params: {
         type: 'object',
@@ -142,22 +228,13 @@ export async function init(): Promise<FastifyInstance> {
       },
       response: {
         200: {
-          type: 'object',
-          properties: {
-            ok: { type: 'boolean' },
-          },
+          $ref: 'responses/success',
         },
         400: {
-          type: 'object',
-          properties: {
-            error: { type: 'string' },
-          },
+          $ref: 'responses/error',
         },
         404: {
-          type: 'object',
-          properties: {
-            error: { type: 'string' },
-          },
+          $ref: 'responses/error',
         },
       },
     },
@@ -193,6 +270,40 @@ export async function init(): Promise<FastifyInstance> {
     },
   });
 
+  server.get('/subscription/:subscriptionId', {
+    schema: {
+      params: {
+        type: 'object',
+        required: ['subscriptionId'],
+        additionalProperties: false,
+        properties: {
+          subscriptionId: { type: 'string' },
+        },
+      },
+      response: {
+        200: {
+          $ref: 'entities/subscription',
+        },
+        404: {
+          $ref: 'responses/error',
+        },
+      },
+    },
+    handler: async (request, reply) => {
+      const { subscriptionId } = request.params as { subscriptionId: string };
+
+      const subscription = await database.subscriptions.findOne(
+        { _id: subscriptionId },
+        { populate: ['customer', 'changes'] },
+      );
+      if (!subscription) {
+        return reply.code(404).send({ error: 'Subscription not found' });
+      }
+
+      await reply.send({ subscription });
+    },
+  });
+
   server.get('/subscription/:subscriptionId/invoice', {
     schema: {
       params: {
@@ -214,23 +325,7 @@ export async function init(): Promise<FastifyInstance> {
         200: {
           oneOf: [
             {
-              type: 'object',
-              properties: {
-                items: {
-                  type: 'array',
-                  items: {
-                    type: 'object',
-                    properties: {
-                      start: { type: 'string' },
-                      end: { type: 'string' },
-                      units: { type: 'number' },
-                      pricePerUnit: { type: 'number' },
-                    },
-                  },
-                },
-                start: { type: 'string' },
-                end: { type: 'string' },
-              },
+              $ref: 'entities/invoice',
             },
             {
               type: 'string',
@@ -238,16 +333,10 @@ export async function init(): Promise<FastifyInstance> {
           ],
         },
         400: {
-          type: 'object',
-          properties: {
-            error: { type: 'string' },
-          },
+          $ref: 'responses/error',
         },
         404: {
-          type: 'object',
-          properties: {
-            error: { type: 'string' },
-          },
+          $ref: 'responses/error',
         },
       },
     },
@@ -271,6 +360,153 @@ export async function init(): Promise<FastifyInstance> {
       } else {
         await reply.send(invoice);
       }
+    },
+  });
+
+  server.post('/customer', {
+    schema: {
+      body: {
+        type: 'object',
+        required: ['email', 'name'],
+        additionalProperties: false,
+        properties: {
+          email: { type: 'string' },
+          name: { type: 'string' },
+        },
+      },
+      response: {
+        200: {
+          $ref: 'entities/customer',
+        },
+        400: {
+          $ref: 'responses/error',
+        },
+      },
+    },
+    handler: async (request, reply) => {
+      const body = request.body as { email: string; name: string };
+
+      const customer = await database.customers.findOne({ email: body.email });
+      if (customer) {
+        return reply.code(400).send({
+          error: 'Customer already exists',
+        });
+      }
+
+      const newCustomer = new Customer({ email: body.email, name: body.name });
+      await database.em.persistAndFlush(newCustomer);
+
+      await reply.send(newCustomer._id);
+    },
+  });
+
+  server.get('/customer', {
+    schema: {
+      querystring: {
+        type: 'object',
+        additionalProperties: false,
+        properties: {
+          email: { type: 'string' },
+        },
+      },
+      response: {
+        200: {
+          type: 'array',
+          items: {
+            $ref: 'entities/customer',
+          },
+        },
+        404: {
+          $ref: 'responses/error',
+        },
+      },
+    },
+    handler: async (request, reply) => {
+      const { email } = request.query as Partial<{ email?: string }>;
+
+      const customers = await database.customers.find({ email });
+
+      await reply.send(customers);
+    },
+  });
+
+  server.put('/customer/:customerId', {
+    schema: {
+      params: {
+        type: 'object',
+        required: ['customerId'],
+        additionalProperties: false,
+        properties: {
+          customerId: { type: 'string' },
+        },
+      },
+      body: {
+        type: 'object',
+        required: ['email', 'name'],
+        additionalProperties: false,
+        properties: {
+          email: { type: 'string' },
+          name: { type: 'string' },
+        },
+      },
+      response: {
+        200: {
+          $ref: 'entities/customer',
+        },
+        404: {
+          $ref: 'responses/error',
+        },
+      },
+    },
+    handler: async (request, reply) => {
+      const { customerId } = request.params as { customerId: string };
+      const { email, name } = request.body as { email: string; name: string };
+
+      const customer = await database.customers.findOne({ _id: customerId });
+      if (!customer) {
+        return reply.code(404).send({ error: 'Customer not found' });
+      }
+
+      customer.email = email;
+      customer.name = name;
+
+      await database.em.persistAndFlush(customer);
+    },
+  });
+
+  server.delete('/customer/:customerId', {
+    schema: {
+      params: {
+        type: 'object',
+        required: ['customerId'],
+        additionalProperties: false,
+        properties: {
+          customerId: { type: 'string' },
+        },
+      },
+      response: {
+        200: {
+          $ref: 'responses/success',
+        },
+        400: {
+          $ref: 'responses/error',
+        },
+        404: {
+          $ref: 'responses/error',
+        },
+      },
+    },
+    handler: async (request, reply) => {
+      const { customerId } = request.params as { customerId: string };
+
+      const customer = await database.customers.findOne({ _id: customerId });
+      if (!customer) {
+        return reply.code(404).send({ error: 'Customer not found' });
+      }
+
+      await database.em.removeAndFlush(customer);
+
+      await reply.send({ ok: true });
     },
   });
 
