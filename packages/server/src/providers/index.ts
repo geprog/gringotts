6130ab9ae7838ff535f8897e5f1c4b56ea4eca85
Anbraten <anton@ju60.de>
2022-07-24 20:31:53 +0200

    chore: remove unused file

--- packages/server/src/providers/index.ts
+++
@@ -1,7 +0,0 @@
-import { config } from '~/config';
-import { Mollie } from '~/providers/mollie';
-import { PaymentProvider } from '~/providers/types';
-
-export const paymentProvider: PaymentProvider = new Mollie({
-  apiKey: config.mollieApiKey,
-});
