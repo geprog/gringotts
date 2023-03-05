e3c06d3f1e6a6dc0a44fd5c323e058db66935b25
Anbraten <anton@ju60.de>
2023-03-05 06:59:57 +0100

    fix: improve migrations and add dev docs

--- packages/server/src/migrations/alter_column_logo_to_text.ts
+++ packages/server/src/migrations/alter_column_logo_to_text.ts
@@ -2,12 +2,20 @@ import { Migration } from '@mikro-orm/migrations';
 
 export class MigrationAlterColumnLogo extends Migration {
   async up(): Promise<void> {
+    if (!(await this.ctx?.schema.hasColumn('project_invoice_data', 'logo'))) {
+      return;
+    }
+
     await this.ctx?.schema.alterTable('project_invoice_data', (table) => {
       table.text('logo').nullable().alter();
     });
   }
 
   async down(): Promise<void> {
+    if (!(await this.ctx?.schema.hasColumn('project_invoice_data', 'logo'))) {
+      return;
+    }
+
     await this.ctx?.schema.alterTable('project_invoice_data', (table) => {
       table.string('logo', 255).alter();
     });
