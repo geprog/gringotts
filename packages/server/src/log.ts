7d133c7592b4bc36c8b823bafec12f8df65ac40b
Anbraten <anton@ju60.de>
2023-07-15 11:07:11 +0200

    fix: avoid bundling of pino-pretty (#50)

--- packages/server/src/log.ts
+++ packages/server/src/log.ts
@@ -2,11 +2,14 @@ import pino from 'pino';
 
 export const log = pino({
   level: process.env.LOG_LEVEL || 'info',
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      translateTime: 'HH:MM:ss Z',
-      ignore: 'pid,hostname',
-    },
-  },
+  transport:
+    process.env.NODE_ENV === 'production'
+      ? undefined
+      : {
+          target: 'pino-pretty',
+          options: {
+            translateTime: 'HH:MM:ss Z',
+            ignore: 'pid,hostname',
+          },
+        },
 });
