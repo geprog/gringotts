20ceb476e0269c6cd59c935eac5e62e68a1edee8
Anbraten <anton@ju60.de>
2023-07-15 10:48:21 +0200

    fix pino-pretty

--- packages/server/src/log.ts
+++ packages/server/src/log.ts
@@ -1,12 +1,17 @@
 import pino from 'pino';
 
-export const log = pino({
-  level: process.env.LOG_LEVEL || 'info',
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      translateTime: 'HH:MM:ss Z',
-      ignore: 'pid,hostname',
-    },
-  },
-});
+export const log =
+  process.env.NODE_ENV === 'production'
+    ? undefined
+    : pino({
+        level: process.env.LOG_LEVEL || 'info',
+        transport: {
+          transport: {
+            target: 'pino-pretty',
+            options: {
+              translateTime: 'HH:MM:ss Z',
+              ignore: 'pid,hostname',
+            },
+          },
+        },
+      });
