f81d418f6b073ada79058be1da5c2d886b2518ae
Anbraten <anton@ju60.de>
2023-10-06 11:23:59 +0200

    fix utils

--- packages/server/src/utils.ts
+++ packages/server/src/utils.ts
@@ -1,23 +1,18 @@
 import dayjs from '~/lib/dayjs';
 
-export function getPeriodFromAnchorDate(someDateInPeriod: Date, _anchorDate: Date): { start: Date; end: Date } {
-  const startDate = Math.min(dayjs(someDateInPeriod).endOf('month').date(), dayjs(_anchorDate).date());
+export function getPeriodFromAnchorDate(someDateInPeriod: Date, anchorDate: Date): { start: Date; end: Date } {
+  const startDate = Math.min(dayjs(someDateInPeriod).endOf('month').date(), dayjs(anchorDate).date());
   let start = dayjs(someDateInPeriod).set('date', startDate).startOf('day');
 
   if (start.isAfter(someDateInPeriod)) {
     // select previous period
-    let end = start.endOf('day');
     const newStart = start.subtract(1, 'month');
-    let date = Math.max(newStart.date(), dayjs(_anchorDate).date());
+    let date = Math.max(newStart.date(), dayjs(anchorDate).date());
     date = Math.min(date, newStart.endOf('month').date());
-    if (date === start.date()) {
-      end = start.subtract(1, 'day').endOf('day');
-    }
     start = newStart.set('date', date);
-    return { start: start.toDate(), end: end.toDate() };
   }
 
-  const endDate = Math.min(start.add(1, 'month').endOf('month').date(), dayjs(_anchorDate).date());
+  const endDate = Math.min(start.add(1, 'month').endOf('month').date(), dayjs(anchorDate).date());
   const end = start.add(1, 'month').endOf('month').set('date', endDate).subtract(1, 'day').endOf('day');
 
   return { start: start.toDate(), end: end.toDate() };
