1d32649ad7d98fc8b7c22b5fd42326875c8ea456
Anbraten <anton@ju60.de>
2023-10-15 11:37:12 +0200

    update migration

--- packages/server/src/migrations/set_next_payment_for_subscriptions.ts
+++ packages/server/src/migrations/set_next_payment_for_subscriptions.ts
@@ -1,16 +1,26 @@
 import { Migration } from '@mikro-orm/migrations';
 
+type Subscription = {
+  _id: string;
+  anchorDate: Date;
+  nextPayment: Date;
+};
+
 export class MigrationSetNextPaymentForSubscriptions extends Migration {
   async up(): Promise<void> {
-    if (!(await this.ctx?.schema.hasColumn('subscription', 'nextPayment'))) {
-      return;
-    }
-
-    await this.ctx?.schema.alterTable('invoice', (table) => {
-      table.date('nextPayment'); // TODO: set value for nextPayment
+    await this.ctx?.schema.alterTable('subscription', (table) => {
+      table.date('nextPayment').defaultTo(new Date());
     });
 
-    // TODO: drop all draft invoices
+    const subscriptions = await this.ctx?.table('subscription').where({ status: 'active' }).select<Subscription[]>();
+    for await (const subscription of subscriptions || []) {
+      const nextPayment = new Date(); // TODO: think about fair nextPayment date and how we can calculate it
+
+      await this.ctx?.table('subscription').where({ _id: subscription._id }).update({ nextPayment });
+    }
+
+    // draft invoices where used to loop due subscriptions
+    await this.ctx?.table('invoice').where({ status: 'draft' }).delete();
   }
 
   async down(): Promise<void> {
