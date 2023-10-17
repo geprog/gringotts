cc6672718bc1e305d7683d88bc1b693091e6687a
Anbraten <anton@ju60.de>
2023-10-17 23:43:52 +0200

    adjust colors

--- packages/app/app.config.ts
+++ packages/app/app.config.ts
@@ -16,10 +16,40 @@ export default defineAppConfig({
         size: 'lg',
       },
     },
-
     button: {
       default: {
         size: 'lg',
+      },
+    },
+    card: {
+      background: 'bg-white dark:bg-zinc-900',
+      ring: 'ring-1 ring-zinc-200 dark:ring-zinc-800',
+      divide: 'divide-y divide-zinc-200 dark:divide-zinc-800',
+    },
+    popover: {
+      background: 'bg-white dark:bg-zinc-900',
+      ring: 'ring-1 ring-zinc-200 dark:ring-zinc-800',
+    },
+    table: {
+      divide: 'divide-y divide-zinc-300 dark:divide-zinc-700',
+      tbody: 'divide-y divide-zinc-200 dark:divide-zinc-800',
+      tr: {
+        selected: 'bg-zinc-50 dark:bg-zinc-800/50',
+        active: 'hover:bg-zinc-50 dark:hover:bg-zinc-800/50 cursor-pointer',
+      },
+      th: {
+        color: 'text-zinc-900 dark:text-white',
+      },
+      td: {
+        color: 'text-zinc-500 dark:text-zinc-400',
+      },
+      loadingState: {
+        label: 'text-sm text-center text-zinc-900 dark:text-white',
+        icon: 'w-6 h-6 mx-auto text-zinc-400 dark:text-zinc-500 mb-4 animate-spin',
+      },
+      emptyState: {
+        label: 'text-sm text-center text-zinc-900 dark:text-white',
+        icon: 'w-6 h-6 mx-auto text-zinc-400 dark:text-zinc-500 mb-4',
       },
     },
   },
