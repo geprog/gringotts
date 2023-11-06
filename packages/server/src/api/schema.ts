5c678d6a7fbee933797fde405292b46b2c42d4ad
Anbraten <anton@ju60.de>
2023-11-06 16:47:47 +0100

    show metadata

--- packages/server/src/api/schema.ts
+++ packages/server/src/api/schema.ts
@@ -136,7 +136,7 @@ export function addSchemas(server: FastifyInstance): void {
     type: 'object',
     properties: {
       _id: { type: 'string' },
-      metadata: { type: 'object' },
+      metadata: { type: 'object', additionalProperties: true },
       anchorDate: { type: 'string' },
       status: { type: 'string' },
       error: { type: 'string' },
